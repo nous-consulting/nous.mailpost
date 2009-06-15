71135b4a85e5cabab29c6480468628a401e1b6a7
Ignas Mikalajūnas <ignas@pow.lt>
2009-06-15 13:26:30 +0300

    Change the url of the project.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Email processor that posts the email received to a url',
     author='Ignas Mikalajunas',
     author_email='ignas@nous.lt',
-    url='',
+    url='http://github.com/Ignas/nous.mailpost/',
     classifiers=["Development Status :: 3 - Alpha",
                  "Environment :: Web Environment",
                  "Topic :: Communications :: Email",
